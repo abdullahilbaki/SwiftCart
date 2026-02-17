ffc88f82b6236dc21eb66d6d1af7339b0deac750
abdullahilbaki <abdullahalbaqi00@gmail.com>
2026-02-17 15:48:18 +0600

    added navbar

---
+++ js/navbar.js
@@ -0,0 +1,51 @@
+const NAV_ITEMS = ["Home", "Products", "About", "Contact"];
+
+const Logo = () =>
+  `<a class="btn btn-ghost text-xl text-primary">SwiftCart</a>`;
+
+const CartIcon = () =>
+  `<a class="btn btn-ghost"><i class="fa-solid fa-cart-shopping"></i></a>`;
+
+const ShowMenuItems = () => {
+  return `
+  <ul class="menu menu-horizontal px-1">
+    ${NAV_ITEMS.map((item) => `<li><a>${item}</a></li>`).join("")}
+  </ul>
+  `;
+};
+
+const ShowMenuItemsMobile = () => {
+  return `
+  <div class="dropdown dropdown-end">
+  <div tabindex="0" role="button" class="btn btn-ghost lg:hidden m-1">
+    <i class="fa-solid fa-bars"></i>
+  </div>
+  <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    ${NAV_ITEMS.map((item) => `<li><a>${item}</a></li>`).join("")}
+  </ul>
+  </div>
+  `;
+};
+
+const Navbar = () => {
+  return `
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+      ${Logo()}
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      ${ShowMenuItems()}
+    </div>
+    <div class="navbar-end">
+      ${CartIcon()}
+      ${ShowMenuItemsMobile()}
+    </div>
+  </div>
+      `;
+};
+
+const navElement = document.getElementById("navbar");
+
+if (navElement) {
+  navElement.innerHTML = Navbar();
+}
