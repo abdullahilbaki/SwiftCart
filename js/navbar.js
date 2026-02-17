fb70c380e770d2fec9e1c43657656fa7958196ca
abdullahilbaki <abdullahalbaqi00@gmail.com>
2026-02-17 17:16:09 +0600

    fixed code formatting in string literals

--- js/navbar.js
+++ js/navbar.js
@@ -8,40 +8,40 @@ const CartIcon = () =>
 
 const ShowMenuItems = () => {
   return `
-  <ul class="menu menu-horizontal px-1 text-lg">
-    ${NAV_ITEMS.map((item) => `<li><a>${item}</a></li>`).join("")}
-  </ul>
+    <ul class="menu menu-horizontal px-1 text-lg">
+      ${NAV_ITEMS.map((item) => `<li><a>${item}</a></li>`).join("")}
+    </ul>
   `;
 };
 
 const ShowMenuItemsMobile = () => {
   return `
-  <div class="dropdown dropdown-end">
-  <div tabindex="0" role="button" class="btn btn-ghost lg:hidden m-1">
-    <i class="fa-solid fa-bars text-xl"></i>
-  </div>
-  <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-    ${NAV_ITEMS.map((item) => `<li><a>${item}</a></li>`).join("")}
-  </ul>
-  </div>
+    <div class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn btn-ghost lg:hidden m-1">
+      <i class="fa-solid fa-bars text-xl"></i>
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      ${NAV_ITEMS.map((item) => `<li><a>${item}</a></li>`).join("")}
+    </ul>
+    </div>
   `;
 };
 
 const Navbar = () => {
   return `
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="navbar-start">
-      ${Logo()}
+    <div class="navbar bg-base-100 shadow-sm">
+      <div class="navbar-start">
+        ${Logo()}
+      </div>
+      <div class="navbar-center hidden lg:flex">
+        ${ShowMenuItems()}
+      </div>
+      <div class="navbar-end">
+        ${CartIcon()}
+        ${ShowMenuItemsMobile()}
+      </div>
     </div>
-    <div class="navbar-center hidden lg:flex">
-      ${ShowMenuItems()}
-    </div>
-    <div class="navbar-end">
-      ${CartIcon()}
-      ${ShowMenuItemsMobile()}
-    </div>
-  </div>
-      `;
+  `;
 };
 
 const navElement = document.getElementById("navbar");
